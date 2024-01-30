b31813f1eff9d8703744d4c035bb4d4ef33284c3
Patrick Smith <pgwsmith@gmail.com>
2024-01-30 16:12:12 +1100

    Use rollup to build

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+const del = require('rollup-plugin-delete')
+const commonjs = require('@rollup/plugin-commonjs')
+
+const entries = [
+  './src/index.ts'
+]
+
+module.exports = [
+  {
+    input: entries,
+    output: [
+      {
+        dir: 'dist',
+        entryFileNames: '[name].mjs',
+        chunkFileNames: '[name]-[hash].mjs',
+        format: 'esm',
+      },
+      {
+        dir: 'dist',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name]-[hash].js',
+        format: 'cjs',
+      },
+    ],
+    external: id =>
+      !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/'),
+    plugins: [del({ targets: 'dist/*' }), commonjs()],
+  },
+]
